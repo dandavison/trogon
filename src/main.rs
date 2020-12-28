75d496bfb7593beb470816343087c40c451f392d
Dan Davison <dandavison7@gmail.com>
2020-12-28 11:20:32 -0500

    Add an HTML endpoint

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,41 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/html")]
+fn html() -> content::Html<&'static str> {
+    content::Html(
+        r#"<!DOCTYPE html>
+<html>
+<head>
+  <title>My first Vue app</title>
+  <script src="https://unpkg.com/vue"></script>
+</head>
+<body>
+  <div id="app">
+    {{ message }}
+  </div>
+
+  <script>
+    var app = new Vue({
+      el: '#app',
+      data: {
+        message: 'Hello Vue!'
+      }
+    })
+  </script>
+</body>
+</html>
+"#,
+    )
+}
+
 #[get("/json")]
 fn json() -> content::Json<&'static str> {
     content::Json(r#"{ "hi": "world" }"#)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index, json]).launch();
+    rocket::ignite()
+        .mount("/", routes![index, html, json])
+        .launch();
 }
