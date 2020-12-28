ffafdfe39d42e1b1750859f74c6f90ae15ca13bb
Dan Davison <dandavison7@gmail.com>
2020-12-28 11:02:37 -0500

    https://rocket.rs/v0.4/guide/getting-started/

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index]).launch();
 }
