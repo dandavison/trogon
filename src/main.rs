f09ed6b1c07bc33d8e4bec4d4109e5b1decdd144
Dan Davison <dandavison7@gmail.com>
2020-12-29 15:07:15 -0500

    Start adding map data

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,22 @@ fn index() -> &'static str {
 
 #[get("/map")]
 fn map() -> Template {
-    let mut context = HashMap::<&str, f64>::new();
+    let mut context = HashMap::<&str, Vec<(String, f64, f64, i32)>>::new();
     let rows = db::get_client()
-        .query("select lat, lon from site where name = 'Villa Azul'", &[])
+        .query(
+            "select id, name, lat, lon from site where name = 'Villa Azul'",
+            &[],
+        )
         .unwrap();
-    context.insert("lat", rows[0].get("lat"));
-    context.insert("lon", rows[0].get("lon"));
+    context.insert(
+        "sites",
+        vec![(
+            rows[0].get("name"),
+            rows[0].get("lat"),
+            rows[0].get("lon"),
+            rows[0].get("id"),
+        )],
+    );
     Template::render("map", context)
 }
 
