8776fbe9dcba272081f65981347a706780673cf6
Dan Davison <dandavison7@gmail.com>
2020-12-30 22:39:33 -0500

    Update schema and data

--- db.sql
+++ db.sql
@@ -1,38 +1,100 @@
 create table site (
-  id integer primary key,
+  id serial primary key,
   name varchar,
   lat float8,
   lon float8
 );
 
-create table tour (
+insert into site values (1, 'Moroco', 0.1444015171331046, -70.96084408873314);
+insert into site values (2, 'Mitú', 1.2539646349060007, -70.23341850460245);
+insert into site values (3, 'Villa Azul', -0.5745422166246894, -72.11438768433418);
+
+------------------------------------------------------------------------------------
+
+create table guide (
+  id integer primary key,
+  name varchar,
+  tour_guide boolean,
+  biography text
+);
+
+insert into guide values (1, 'Jose Castaño', true, 'Jose has been studying birds since 2001 when he became interested in the protection of the Yellow-eared Parrot. Jose was a founding member of Proaves, and helped to establish many successful nature reserves. Jose has been guiding professionally since 2008.');
+insert into guide values (2, 'Miguel', false, 'Miguel has many years of experience of field identification of forest birds in the sites close to Mitú. He uses both English and scientific names of bird species.');
+insert into guide values (3, 'Florencio', false, null);
+insert into guide values (4, 'Anthony', false, null);
+
+------------------------------------------------------------------------------------
+
+create table site_guide (
+  id integer primary key,
+  site integer references site,
+  guide integer references guide,
+  unique (site, guide)
+);
+
+insert into site_guide values (1, 2, 2);
+insert into site_guide  values (2, 2, 3);
+insert into site_guide  values (3, 3, 4);
+
+------------------------------------------------------------------------------------
+
+create table language (
   id integer primary key,
   name varchar
 );
 
-create table site_day (
+------------------------------------------------------------------------------------
+
+insert into language values (1, 'Spanish');
+insert into language values (2, 'English');
+
+create table guide_language (
   id integer primary key,
-  tour integer references tour,
-  site integer references site,
-  day integer,
-  unique (site, day)
+  guide integer references guide (id),
+  language integer references language (id)
 );
 
-create table site_guide (
-  id integer primary key,
-  name varchar,
-  site integer references site,
-  unique (id, site)
-);
+insert into guide_language values (1, 1, 1);
+insert into guide_language values (2, 1, 2);
+insert into guide_language values (3, 2, 1);
 
-create table site_day_guide (
-  id integer primary key,
-  site integer references site,
-  site_guide integer references site_guide,
-  day integer,
-  foreign key (site, day) references site_day (site, day),
-  foreign key (site_guide, site) references site_guide (id, site)
-);
+------------------------------------------------------------------------------------
+
+-- create table tour (
+--   id integer primary key,
+--   name varchar
+-- );
+
+-- insert into tour values (1, 'Araracuara Jan 2021');
+
+------------------------------------------------------------------------------------
+
+-- create table site_day (
+--   id integer primary key,
+--   tour integer references tour,
+--   site integer references site,
+--   day integer,
+--   unique (site, day)
+-- );
+
+-- insert into site_day (id, tour, site, day) values (1, 1, 1, 1);
+-- insert into site_day (id, tour, site, day) values (2, 1, 1, 2);
+
+------------------------------------------------------------------------------------
+
+-- create table day_site_guide (
+--   id integer primary key,
+--   site integer references site,
+--   site_guide integer references site_guide,
+--   day integer,
+--   foreign key (site, day) references site_day (site, day),
+--   foreign key (site_guide, site) references site_guide (id, site)
+-- );
+
+-- insert into site_day_guide values (1, 1, 1, 1);
+-- insert into site_day_guide values (2, 1, 2, 2);
+
+------------------------------------------------------------------------------------
 
 create table ebird_hotspot (
   locId varchar primary key,
@@ -60,35 +122,3 @@ create table ebird_hotspot_species (
   species varchar references ebird_species,
   locId varchar references ebird_hotspot
 );
-
--- site
-insert into site (id, name, lat, lon)
-values (1, 'Villa Azul', -0.5745422166246894, -72.11438768433418);
-insert into site (id, name, lat, lon)
-values (2, 'Moroco', 0.1444015171331046, -70.96084408873314);
-
--- site_guide
--- insert Fred and Anthony as guide at Villa Azul
--- with site as (select id from site where name = "Villa Azul")
-insert into site_guide (id, name, site)
-values (1, 'Fred', 1);
-insert into site_guide  (id, name, site)
-values (2, 'Anthony', 1);
-
--- tour
-insert into tour (id, name)
-values (1, 'Araracuara Jan 2021');
-
--- site_day
-insert into site_day (id, tour, site, day)
-values (1, 1, 1, 1);
-insert into site_day (id, tour, site, day)
-values (2, 1, 1, 2);
-
--- site_day_guide
--- Fred on day 1
-insert into site_day_guide (id, site, site_guide, day)
-values (1, 1, 1, 1);
--- Anthony on day 2
-insert into site_day_guide (id, site, site_guide, day)
-values (2, 1, 2, 2);
