d7f6709ba39fdf4456ace720de48dd17304c4778
Dan Davison <dandavison7@gmail.com>
2021-01-01 17:16:41 -0500

    Init vue app

---
+++ ui/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app-component"></div>
+</template>
+
+<script>
+export default {
+  setup() {
+    console.log("app setup, fetching /api/sites JSON...");
+    fetch("http://localhost:8000/api/sites").then((response) => {
+      response.json().then((sites) => console.log(sites));
+    });
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
