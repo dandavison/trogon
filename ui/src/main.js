8fde9d7661dd6769a3bd8ebf0bc9b5911c62080a
Catherine Noll <noll.catherine@gmail.com>
2021-01-02 10:33:15 -0500

    Add Beaufy

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,5 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy, {
+  defaultIconPack: 'fas',
+  defaultContainerElement: '#content',
+})
 
 new Vue({
   render: (h) => h(App),
