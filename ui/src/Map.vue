81a2866fddbdec5647f085fd95e3a407669d8dfd
Dan Davison <dandavison7@gmail.com>
2021-01-03 10:09:21 -0500

    Revert "Use google satellite images"
    This reverts commit e38f0248a2a10dfa2c0165f20627e89510aa4425.

--- ui/src/Map.vue
+++ ui/src/Map.vue
@@ -67,10 +67,19 @@ export default {
 function createMap(sites) {
   // -> Map
   var mymap = L.map("map").setView([sites[0].lat, sites[0].lng], 5);
-  L.tileLayer("http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}", {
-    maxZoom: 20,
-    subdomains: ["mt0", "mt1", "mt2", "mt3"],
-  }).addTo(mymap);
+
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+    {
+      maxZoom: 18,
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+    }
+  ).addTo(mymap);
   return mymap;
 }
 
