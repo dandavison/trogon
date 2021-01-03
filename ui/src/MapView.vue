df38d3d2b8062a5be3a1620b57ee5c4d583a72f2
Dan Davison <dandavison7@gmail.com>
2021-01-02 21:21:41 -0500

    HACK: force data reference to change, not just internal contents

--- ui/src/MapView.vue
+++ ui/src/MapView.vue
@@ -27,12 +27,15 @@ export default {
     },
     changeShowTrip: function (newVal, trip) {
       console.log("MapView show trip: ", newVal, trip.name);
+      let newTrips = [];
       for (let _trip of this.trips) {
         if (_trip.id === trip.id) {
           console.log("MapView updating visibility", newVal, trip.name);
           _trip["isVisible"] = newVal;
+          newTrips.push(_trip);
         }
       }
+      this.trips = newTrips;
     },
     showTrips: function (trips) {
       console.log("MapView: setting trips", trips);
