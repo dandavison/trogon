e3d0d68533f33ace90763b9ddf30e8fb42d2471a
Dan Davison <dandavison7@gmail.com>
2021-01-09 13:50:16 -0500

    Add Trip type to trip switch components

--- ui/src/TripSwitch.vue
+++ ui/src/TripSwitch.vue
@@ -7,23 +7,24 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue, { PropType } from "vue";
 import VueI18n from "vue-i18n";
 Vue.use(VueI18n);
 
 import eventBus from "./event-bus";
+import { Trip } from "types";
 
 export default Vue.extend({
   data() {
     return {
-      isSwitched: false
+      isSwitched: false,
     };
   },
-  props: ["trip"],
+  props: { trip: Object as PropType<Trip> },
   watch: {
-    isSwitched: function(newVal) {
+    isSwitched: function (newVal) {
       eventBus.$emit("change:show-trip", newVal, this.trip);
-    }
-  }
+    },
+  },
 });
 </script>
--- ui/src/TripSwitches.vue
+++ ui/src/TripSwitches.vue
@@ -7,17 +7,18 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue, { PropType } from "vue";
 import VueI18n from "vue-i18n";
 Vue.use(VueI18n);
 
 import TripSwitch from "./TripSwitch.vue";
+import { Trip } from "types";
 
 export default Vue.extend({
   data() {
     return {};
   },
-  props: ["trips"],
-  components: { TripSwitch }
+  props: { trips: Array as PropType<Trip[]> },
+  components: { TripSwitch },
 });
 </script>
