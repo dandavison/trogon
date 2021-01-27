a98b23f1cef896fcc9ab6877f7066d37da15b812
Dan Davison <dandavison7@gmail.com>
2021-01-27 17:15:50 -0500

    Delete server-side HTML endpoints

--- src/main.rs
+++ src/main.rs
@@ -13,102 +13,8 @@ mod queries;
 use rocket_contrib::serve::StaticFiles;
 use std::process;
 
-use rocket_contrib::templates::Template;
-use serde::Serialize;
 use structopt::clap::AppSettings::{ColorAlways, ColoredHelp, DeriveDisplayOrder};
 use structopt::StructOpt;
-
-#[derive(Serialize)]
-struct SiteContext {
-    site: models::Site,
-    guides: Vec<models::Guide>,
-}
-
-#[get("/site/<id>")]
-fn site(id: i32) -> Template {
-    let mut dbclient = db::get_client();
-    let site: models::Site = dbclient
-        .query("select * from site where id = $1", &[&id])
-        .unwrap()
-        .remove(0)
-        .into();
-    let guides = dbclient
-        .query(
-            "
-select * from guide g
-inner join site_guide sg on sg.guide = g.id
-where sg.site = $1
-",
-            &[&id],
-        )
-        .unwrap();
-    Template::render(
-        "site",
-        SiteContext {
-            site,
-            guides: models::serializable(guides),
-        },
-    )
-}
-
-#[derive(Serialize)]
-struct EbirdHotspotContext {
-    hotspot: models::EbirdHotspot,
-    species: Vec<models::EbirdSpecies>,
-}
-
-#[get("/hotspot/<loc_id>")]
-fn ebird_hotspot(loc_id: String) -> Template {
-    let mut dbclient = db::get_client();
-    let context = EbirdHotspotContext {
-        hotspot: dbclient
-            .query_one(
-                "
-select * from ebird_hotspot where locId = $1
-",
-                &[&loc_id],
-            )
-            .unwrap()
-            .into(),
-        species: models::serializable(
-            dbclient
-                .query(
-                    "
-select * from ebird_species es
-inner join ebird_hotspot_species ehs on ehs.species = es.speciesCode
-where ehs.locId = $1
-",
-                    &[&loc_id],
-                )
-                .unwrap(),
-        ),
-    };
-    Template::render("ebird_hotspot", context)
-}
-
-#[derive(Serialize)]
-struct TripContext {
-    trip: models::Trip,
-    days: Vec<queries::trip_site_day_with_site_info::Row>,
-}
-
-#[get("/trip/<trip_id>")]
-fn trip(trip_id: i32) -> Template {
-    let mut dbclient = db::get_client();
-    let context = TripContext {
-        trip: dbclient
-            .query_one(
-                "
-select * from trip where id = $1
-",
-                &[&trip_id],
-            )
-            .unwrap()
-            .into(),
-        days: queries::trip_site_day_with_site_info::query(trip_id),
-    };
-    Template::render("trip", context)
-}
 
 #[derive(StructOpt)]
 #[structopt(
@@ -153,13 +59,11 @@ fn main() -> std::io::Result<()> {
         process::exit(ebird::load::load_species()?)
     } else {
         rocket::ignite()
-            .mount("/", routes![site, ebird_hotspot, trip])
             .mount(
                 "/api",
                 routes![api::guides, api::ebird_hotspots, api::sites, api::trips],
             )
             .mount("/", StaticFiles::from("ui/dist"))
-            .attach(Template::fairing())
             .attach(cors::CORS())
             .launch();
     }
--- src/queries/mod.rs
+++ src/queries/mod.rs
@@ -1,7 +1,5 @@
-pub mod ebird_hotspot_with_species_count;
 pub mod ebird_hotspots;
 pub mod guides;
 pub mod habitats;
 pub mod sites;
-pub mod trip_site_day_with_site_info;
 pub mod trips;
