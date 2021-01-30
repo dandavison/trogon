baa4b2470a3918b48505bd5993fab1761ba27d05
Dan Davison <dandavison7@gmail.com>
2021-01-29 23:02:03 -0500

    Restrict xeno-canto query to song & high quality

--- ui/src/sounds/xeno-canto.ts
+++ ui/src/sounds/xeno-canto.ts
@@ -6,7 +6,7 @@ const XENO_CANTO_API_URL = `${process.env.VUE_APP_SERVER_URL}/xeno-canto/`;
 
 function getXenoCantoRecordings(query: string): XenoCantoRecording[] {
   const xcData = fetchJSONObjectSynchronously(
-    `${XENO_CANTO_API_URL}?query=${query}`
+    `${XENO_CANTO_API_URL}?query=${query}+q:A`
   );
   return xcData.recordings || [];
 }
@@ -16,19 +16,22 @@ export function getRecordings(species: EbirdSpecies): Recording[] {
   const familyEn = ebirdSpecies.getFamilyEn(species);
   const genus = ebirdSpecies.getGenus(species);
   const speciesSci = ebirdSpecies.getSpeciesSci(species);
+  const speciesEn = ebirdSpecies.getSpeciesEn(species);
 
   const query = `${genus}+${speciesSci}+gen:${genus}`;
   var recordings = [];
   for (let raw of getXenoCantoRecordings(query)) {
-    recordings.push({
-      url: raw.file,
-      familySci: familySci,
-      familyEn: familyEn,
-      genus: raw.gen,
-      speciesSci: raw.sp,
-      speciesEn: raw.en,
-      raw: raw
-    });
+    if (raw.type === "song") {
+      recordings.push({
+        url: raw.file,
+        familySci: familySci,
+        familyEn: familyEn,
+        genus: genus,
+        speciesSci: speciesSci,
+        speciesEn: speciesEn,
+        raw: raw
+      });
+    }
   }
   return recordings;
 }
