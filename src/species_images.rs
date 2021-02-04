ae3f062eb0d8285ec5400263853bf40190f6c936
Dan Davison <dandavison7@gmail.com>
2021-02-04 18:14:00 -0500

    Cache scraped images

--- src/species_images.rs
+++ src/species_images.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use crate::db;
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SpeciesImage {
     species: String,
@@ -7,22 +9,45 @@ pub struct SpeciesImage {
 }
 
 pub fn get_images(species: Vec<&str>) -> Vec<SpeciesImage> {
-    let mut urls = Vec::new();
+    let mut images = Vec::new();
     for sp in species.iter() {
-        urls.push(SpeciesImage {
-            species: sp.to_string(),
-            urls: get_images_for_one_species(sp),
-        })
+        images.push(get_image(sp))
     }
-    urls
+    images
 }
 
-fn get_images_for_one_species(species: &str) -> Vec<String> {
+fn get_image(species: &str) -> SpeciesImage {
     let html_url = format!(
         "https://en.wikipedia.org/w/index.php?title={}",
         species.replace(" ", "_")
     );
-    let html = reqwest::blocking::get(&html_url).unwrap().text().unwrap();
+    let cache_key = format!("scrape:{}", html_url);
+    let mut dbclient = db::get_client();
+    let img_url = match dbclient
+        .query_one("select value from cache where key = $1", &[&cache_key])
+        .map(|row| row.get("value"))
+    {
+        Ok(value) => value,
+        Err(_) => {
+            let img_url = _scrape_wikipedia_image(&html_url);
+            dbclient
+                .execute(
+                    "insert into cache (key, value) values ($1, $2);",
+                    &[&cache_key, &img_url],
+                )
+                .unwrap();
+
+            img_url
+        }
+    };
+    SpeciesImage {
+        species: species.to_string(),
+        urls: vec![img_url],
+    }
+}
+
+fn _scrape_wikipedia_image(html_url: &str) -> String {
+    let html = reqwest::blocking::get(html_url).unwrap().text().unwrap();
     let doc = scraper::Html::parse_document(&html);
     let table_selector = scraper::Selector::parse("table.infobox").unwrap();
     let table = doc.select(&table_selector).next().unwrap();
@@ -32,5 +57,6 @@ fn get_images_for_one_species(species: &str) -> Vec<String> {
     if !url.starts_with("http") {
         url = format!("http:{}", url);
     }
-    vec![url]
+    println!("scraped image: {}", &url);
+    url
 }
