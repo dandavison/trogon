cc3722de3a4ccc5d1065570ee75d366f225287ff
Dan Davison <dandavison7@gmail.com>
2021-02-05 18:08:46 -0500

    Handle errors in rust scraper

--- src/species_images.rs
+++ src/species_images.rs
@@ -84,12 +84,37 @@ fn _get_images_for_one_species(species: &str) -> SpeciesImages {
 }
 
 fn _scrape_wikipedia_image(html_url: &str) -> String {
-    let html = reqwest::blocking::get(html_url).unwrap().text().unwrap();
+    let response = match reqwest::blocking::get(html_url) {
+        Ok(r) => r,
+        Err(err) => {
+            eprintln!("Error scraping {}: {}", html_url, err);
+            return "".to_string();
+        }
+    };
+    let html = match response.text() {
+        Ok(h) => h,
+        Err(err) => {
+            eprintln!("Error scraping {}: {}", html_url, err);
+            return "".to_string();
+        }
+    };
     let doc = scraper::Html::parse_document(&html);
     let table_selector = scraper::Selector::parse("table.infobox").unwrap();
-    let table = doc.select(&table_selector).next().unwrap();
+    let table = match doc.select(&table_selector).next() {
+        Some(t) => t,
+        None => {
+            eprintln!("Error scraping {}: table not found in HTML", html_url);
+            return "".to_string();
+        }
+    };
     let img_selector = scraper::Selector::parse("img").unwrap();
-    let img = table.select(&img_selector).next().unwrap();
+    let img = match table.select(&img_selector).next() {
+        Some(i) => i,
+        None => {
+            eprintln!("Error scraping {}: img not found in HTML", html_url);
+            return "".to_string();
+        }
+    };
     let mut url = img.value().attr("src").unwrap().to_string();
     if !url.starts_with("http") {
         url = format!("http:{}", url);
