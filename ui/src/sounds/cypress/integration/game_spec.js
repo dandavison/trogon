db18da35480ed31e029dec6d9e643e8e6fae2adb
Dan Davison <dandavison7@gmail.com>
2021-02-10 09:48:13 -0500

    Add cypress tests for form field autofill

--- ui/src/sounds/cypress/integration/game_spec.js
+++ ui/src/sounds/cypress/integration/game_spec.js
@@ -1,15 +1,51 @@
 /// <reference types="Cypress" />
 
-describe("Game", () => {
-  it("English family name autofills on entering scientific family name", () => {
+describe("Form field autofill behaviour", () => {
+  before(() => {
     cy.visit("/sounds/challenge?location=L5845383");
     cy.contains("Family (scientific)");
     cy.contains("Family (English)");
-    cy.get("#familyEnField input").should(
-      "not.have.value",
-      "Pigeons and Doves"
-    );
+    cy.contains("Genus");
+    cy.contains("Species (scientific)");
+    cy.contains("Species (English)");
+    cy.get("input").should("have.value", "");
+  });
+
+  afterEach(() => {
+    cy.contains("Clear").click();
+    cy.get("input").should("have.value", "");
+  });
+
+  it("English family autofills on entering scientific family", () => {
     cy.get("#familySciField input").type("Columbidae");
     cy.get("#familyEnField input").should("have.value", "Pigeons and Doves");
+  });
+
+  it("Scientific family autofills on entering english family", () => {
+    cy.get("#familyEnField input").type("Pigeons and Doves");
+    cy.get("#familySciField input").should("have.value", "Columbidae");
+  });
+
+  it("Family names autofill on entering genus", () => {
+    cy.get("#genusField input").type("Patagioenas");
+    cy.get("#familySciField input").should("have.value", "Columbidae");
+    cy.get("#familyEnField input").should("have.value", "Pigeons and Doves");
+  });
+
+  it("Family, genus and English species autofill on entering scientific species", () => {
+    cy.get("#speciesSciField input").type("Patagioenas subvinacea");
+    cy.get("#familySciField input").should("have.value", "Columbidae");
+    cy.get("#familyEnField input").should("have.value", "Pigeons and Doves");
+    cy.get("#speciesEnField input").should("have.value", "Ruddy Pigeon");
+  });
+
+  it("Family, genus and scientific species autofill on entering English species", () => {
+    cy.get("#speciesEnField input").type("Ruddy Pigeon");
+    cy.get("#familySciField input").should("have.value", "Columbidae");
+    cy.get("#familyEnField input").should("have.value", "Pigeons and Doves");
+    cy.get("#speciesSciField input").should(
+      "have.value",
+      "Patagioenas subvinacea"
+    );
   });
 });
