d5cf30ecb7554d66f18a634321a17b9880f76167
Dan Davison <dandavison7@gmail.com>
2021-02-15 11:13:54 -0500

    Remove locale selector

--- ui/src/trogon/Navbar.vue
+++ ui/src/trogon/Navbar.vue
@@ -10,30 +10,16 @@
       <b-navbar-item href="#" @click="showControlPanel">
         <i class="fas fa-cog"></i>
       </b-navbar-item>
-
-      <b-navbar-item>
-        <locale-selector />
-      </b-navbar-item>
     </template>
   </b-navbar>
 </template>
 
-<i18n>
-{
-  "en": {},
-  "es": {}
-}
-</i18n>
 
 <script lang="ts">
 import Vue from "vue";
-import VueI18n from "vue-i18n";
-Vue.use(VueI18n);
-import LocaleSelector from "../LocaleSelector.vue";
 import eventBus from "./event-bus";
 
 export default Vue.extend({
-  components: { LocaleSelector },
   methods: {
     showControlPanel(): void {
       eventBus.$emit("control-panel:show");
