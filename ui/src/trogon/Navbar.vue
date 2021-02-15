73279a5c10ba6b145568b710f64747382e88b0b3
Dan Davison <dandavison7@gmail.com>
2021-02-15 11:50:04 -0500

    Add image to navbar

--- ui/src/trogon/Navbar.vue
+++ ui/src/trogon/Navbar.vue
@@ -1,5 +1,16 @@
 <template>
   <b-navbar>
+    <template slot="brand">
+      <b-navbar-item>
+        <router-link to="/trogon">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Trogon_collaris_%28Trog%C3%B3n_collarejo%29_%2814027165535%29.jpg/320px-Trogon_collaris_%28Trog%C3%B3n_collarejo%29_%2814027165535%29.jpg"
+            alt="Trogon"
+          />
+        </router-link>
+      </b-navbar-item>
+    </template>
+
     <template slot="end">
       <b-navbar-item tag="div">
         <router-link to="/trogon">
