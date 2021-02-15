3009b1d1ea52e0a6ab51781c41b0eb912a8ab7b7
Dan Davison <dandavison7@gmail.com>
2021-02-15 11:50:15 -0500

    Add navbar to map view

--- ui/src/trogon/Root.vue
+++ ui/src/trogon/Root.vue
@@ -1,5 +1,6 @@
 <template>
   <section>
+    <navbar />
     <map-component :center="[0.0, 0.0]" />
   </section>
 </template>
@@ -9,9 +10,10 @@ import { EbirdHotspot } from "types";
 import Vue from "vue";
 
 import MapComponent from "./Map.vue";
+import Navbar from "./Navbar.vue";
 
 export default Vue.extend({
-  components: { MapComponent },
+  components: { MapComponent, Navbar },
   data() {
     return {
       ebirdHotSpots: [] as EbirdHotspot[],
