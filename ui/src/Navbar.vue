907dcd6ad5287ce1e0f35c3aec4b5f7caeacddc8
Dan Davison <dandavison7@gmail.com>
2021-02-17 21:23:32 -0500

    Fix navbar links

--- ui/src/Navbar.vue
+++ ui/src/Navbar.vue
@@ -2,7 +2,7 @@
   <b-navbar>
     <template slot="brand">
       <b-navbar-item>
-        <router-link to="/trogon">
+        <router-link to="/">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Trogon_collaris_%28Trog%C3%B3n_collarejo%29_%2814027165535%29.jpg/320px-Trogon_collaris_%28Trog%C3%B3n_collarejo%29_%2814027165535%29.jpg"
             alt="Trogon"
@@ -11,7 +11,7 @@
       </b-navbar-item>
 
       <b-navbar-item tag="div">
-        <router-link to="/trogon">
+        <router-link to="/">
           <i class="fas fa-globe-americas"></i>
         </router-link>
       </b-navbar-item>
