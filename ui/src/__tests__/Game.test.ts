741b9c7e7a1e085c35f9c471b2362bfe96abea85
Dan Davison <dandavison7@gmail.com>
2021-02-20 08:26:29 -0500

    Test incorrect entry does not reveal answer

--- ui/src/__tests__/Game.test.ts
+++ ui/src/__tests__/Game.test.ts
@@ -9,9 +9,25 @@ type GameFormFieldInstance = InstanceType<typeof GameFormField>;
 
 import RevealArea from "../RevealArea.vue";
 import { ebirdSpecies as ES } from "../ebird";
-import { recording, correctSpecies } from "./fixtures";
+import { recording, correctSpecies, incorrectFamily } from "./fixtures";
 
 describe("Game", () => {
+  test("Truth is not revealed on entering incorrect English species name", async () => {
+    const gameWrapper: Wrapper<GameInstance> = factory();
+    const gameFormWrapper = gameWrapper.findComponent(GameForm) as Wrapper<
+      GameFormInstance
+    >;
+    expect(gameFormWrapper.exists()).toBe(true);
+    const speciesEnFieldWrapper = gameFormWrapper.findComponent({
+      ref: "speciesEnField"
+    }) as Wrapper<GameFormFieldInstance>;
+    expect(speciesEnFieldWrapper.exists()).toBe(true);
+    expect(gameWrapper.findComponent(RevealArea).exists()).toBe(false);
+    speciesEnFieldWrapper.setData({ answer: ES.getSpeciesEn(incorrectFamily) });
+    await gameWrapper.vm.$nextTick();
+    expect(gameWrapper.findComponent(RevealArea).exists()).toBe(false);
+  });
+
   test("Truth is revealed on entering correct English species name", async () => {
     const gameWrapper: Wrapper<GameInstance> = factory();
     const gameFormWrapper = gameWrapper.findComponent(GameForm) as Wrapper<
