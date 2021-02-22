2eeeb7f2265dfa14b7eaee844073ee6177044420
Dan Davison <dandavison7@gmail.com>
2021-02-21 23:05:27 -0500

    npm build

---
+++ ui/src/Example.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>hello __{{ msg }}__</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    msg: String,
+  },
+});
+</script>
