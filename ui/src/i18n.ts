1f5376f605972b4254f6825205dac3e391038830
Dan Davison <dandavison7@gmail.com>
2021-02-26 19:49:13 -0500

    Detect locale from browser environment

--- ui/src/i18n.ts
+++ ui/src/i18n.ts
@@ -23,7 +23,8 @@ function loadLocaleMessages(): LocaleMessages {
 }
 
 export default new VueI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || "en",
+  locale:
+    process.env.VUE_APP_I18N_LOCALE || navigator.language.split("-")[0] || "en",
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || "en",
   messages: loadLocaleMessages()
 });
